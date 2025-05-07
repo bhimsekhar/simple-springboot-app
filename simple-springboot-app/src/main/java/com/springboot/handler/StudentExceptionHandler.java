c782d21619574651c378ebf800d5f52f90e72795
bhimsekhar <bhimsekhar@users.noreply.github.com>
2025-05-07 19:02:46 +0530

    Update StudentExceptionHandler.java

--- simple-springboot-app/src/main/java/com/springboot/handler/StudentExceptionHandler.java
+++ simple-springboot-app/src/main/java/com/springboot/handler/StudentExceptionHandler.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+import java.util.HashMap;
 import com.springboot.exception.InvalidFieldException;
 import com.springboot.exception.InvalidHeaderFieldException;
 
